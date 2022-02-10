1fc8e192437ce97a7b69cbb92fc31f30e2b1c7aa
tkddn2515 <tkddn2515@gmail.com>
2022-02-10 16:30:28 +0900

    Feat graphql use guide

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
 
-function App() {
+const App = () => {
+  const GET_DOGS = gql`
+  query {
+    movies(rating:8.5){
+      id
+      title
+      rating
+    }
+}
+`
+;
+
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  console.log("data", data);
+}
+
+Dogs(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
